06405d5e52eff56c2c32d77025c8669bf589d6e6
Piergiorgio <piergiorgio.bertolini@marconirovereto.it>
2018-10-23 22:01:39 +0200

    Gestito Layout

--- src/cerchioanimato/Cerchio.java
+++ src/cerchioanimato/Cerchio.java
@@ -7,39 +7,34 @@ import javax.swing.*;
 public class Cerchio extends JPanel implements ActionListener {
 
     final int SIZE_PANEL = 50;
-    Timer timer = new Timer(20, this);
+    Timer timer = new Timer(10, this);
     int x = 0, y = 0, dx = 2, dy = 2;
 
     public Cerchio() {
-        setBackground(Color.YELLOW);
+        setBackground(Color.PINK);
         setPreferredSize(new Dimension(SIZE_PANEL, SIZE_PANEL));
         timer.start();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawOval(x, y, SIZE_PANEL, SIZE_PANEL);
+        g.setColor(Color.YELLOW);
+        g.fillOval(x, y, SIZE_PANEL, SIZE_PANEL);
     }
 
     public void sposta() {
-        if (y + dy + SIZE_PANEL + 42 > Finestra.SIZE_FRAME_Y) {
-            dy = -dy;
-        }
-        if (y + dy < 0) {
-            dy = -dy;
-        }
-        if (x + dx + SIZE_PANEL + 10 > Finestra.SIZE_FRAME_X) {
+        if (x < 0 || x + SIZE_PANEL >= Finestra.SIZE_FRAME_X - 22) {
             dx = -dx;
         }
-        if (x + dx < 0) {
-            dx = -dx;
+        if (y < 0 || y + SIZE_PANEL >= Finestra.SIZE_FRAME_Y - 42) {
+            dy = -dy;
         }
-        x += dx;
+        x += dx; 
         y += dy;
+        repaint();
     }
 
     public void actionPerformed(ActionEvent e) {
         sposta();
-        repaint();
     }
 }
--- src/cerchioanimato/Finestra.java
+++ src/cerchioanimato/Finestra.java
@@ -4,18 +4,22 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Finestra extends JFrame {
+
     public static final int SIZE_FRAME_X = 600;
     public static final int SIZE_FRAME_Y = 500;
+
     public Finestra() {
         super("Vogliamo vederlo scorrere");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(SIZE_FRAME_X,SIZE_FRAME_Y));
-        
+        setPreferredSize(new Dimension(SIZE_FRAME_X, SIZE_FRAME_Y));
+        getContentPane().setBackground(Color.PINK);
+        setLayout(new BorderLayout()); // layout di default
         Cerchio c = new Cerchio();
-        add(c);
+        add(c, BorderLayout.CENTER);
         pack();
         setVisible(true);
-    } 
+    }
+
     public static void test() {
         Finestra f = new Finestra();
     }
